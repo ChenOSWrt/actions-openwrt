5cd09d932377209a52ac050d8d235ec9e7910245
BJ <110514714+ChenOSWrt@users.noreply.github.com>
2026-05-11 12:27:13 +0800

    Update Part2_Custom.sh

--- Part2_Custom.sh
+++ Part2_Custom.sh
@@ -6,9 +6,6 @@
 #
 # Copyright (c) 2026 ChenOSWrt
 #
-# 本项目为免费开源软件，遵循 MIT 开源许可证协议。
-# 详细许可条款请参阅仓库内 LICENSE 文件。
-#
 
 # 修改路由器默认主机名（设备名称）
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
